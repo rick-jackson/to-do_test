1b5d93174a0099a7e9d57e89dfbb64d04165ac81
rick-jackson <vitaliisekreta@gmail.com>
2024-01-21 13:06:43 +0200

    test

--- src/common/configs/tasksAnimation.ts
+++ src/common/configs/tasksAnimation.ts
@@ -1,10 +1,15 @@
 export const tasksAnimationsConfig = {
   from: { opacity: 0, transform: "translateX(100%)", transition: "0.3s" },
-  enter: { opacity: 1, transform: "translateX(0%)", transition: "0.3s" },
+  enter: {
+    opacity: 1,
+    transform: "translateX(0%)",
+    transition: "0.3s",
+  },
   leave: {
     opacity: 0,
     transform: "translateX(100%)",
     transition: "0.3s",
+    height: 0,
   },
   config: { duration: 500, friction: 10, tension: 400 },
   unique: true,
